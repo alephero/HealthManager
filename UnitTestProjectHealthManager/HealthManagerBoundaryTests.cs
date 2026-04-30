3d14c6348ed981692d47b43ba41b314eac0cc7fa
Ulyana <sashavov10@gmail.com>
2026-04-30 06:05:12 +0300

    Модульные тесты граничных значений

--- UnitTestProjectHealthManager/HealthManagerBoundaryTests.cs
+++ UnitTestProjectHealthManager/HealthManagerBoundaryTests.cs
@@ -7,8 +7,45 @@ namespace UnitTestProjectHealthManager
     public class HealthManagerBoundaryTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TrackActivity_ZeroDuration_DoesNotCrash()
         {
+            var manager = new HealthManager.HealthManager();
+            manager.TrackActivity("Rest", 0);
+        }
+
+        [TestMethod]
+        public void TrackActivity_NegativeDuration_DoesNotCrash()
+        {
+            var manager = new HealthManager.HealthManager();
+            manager.TrackActivity("Invalid", -10);
+        }
+
+        [TestMethod]
+        public void TrackNutrition_ZeroCalories_DoesNotCrash()
+        {
+            var manager = new HealthManager.HealthManager();
+            manager.TrackNutrition("Water", 0);
+        }
+
+        [TestMethod]
+        public void TrackNutrition_NegativeCalories_DoesNotCrash()
+        {
+            var manager = new HealthManager.HealthManager();
+            manager.TrackNutrition("BadFood", -100);
+        }
+
+        [TestMethod]
+        public void TrackSleep_EmptyDate_DoesNotCrash()
+        {
+            var manager = new HealthManager.HealthManager();
+            manager.TrackSleep("", 8);
+        }
+
+        [TestMethod]
+        public void TrackSleep_ZeroHours_DoesNotCrash()
+        {
+            var manager = new HealthManager.HealthManager();
+            manager.TrackSleep("2024-01-15", 0);
         }
     }
 }
